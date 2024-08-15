f278e5047a14fad1197793009e8ed10695dddb04
earnestsnow-xiaowei <zhaoxiwei@qq.com>
2024-08-15 14:58:38 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// config.js
+const config = {
+    password: "fhqqDMHWFN05.15",  // 设置访问密码
+    salts: {
+        "paintx4.0.6": "xiaoWEI05.15PaintX4.0.6",
+        "paintx4.0.8": "xiaoWEI05.15PaintX4.0.8",
+        "colorfianle2.7.4": "xiaoWEI05.15colorFINALE2.7.4",
+        "colorfianle2.8.1": "xiaoWEI05.15colorFINALE2.8.1",
+        "colorfianle2.9.0": "xiaoWEI05.15colorFINALE2.9.0",
+        "colorfianle2.9.1": "xiaoweicolorfinale2.9.1",
+        "MotionVFX": "xiaoWEI05.15MotionVFX",
+        "neatvideo(达芬奇)": "xiaoWEI05.15NEATVIDEO5.6.5",
+        "neatvideo(FCPX)": "xiaoWEI05.15NEATVIDEO5.6.5FCPX",
+        "neatvideo(PR)": "xiaoWEI05.15NEATVIDEO5.6.5PR",
+        "neatvideo(AE)": "xiaoWEI05.15NEATVIDEO5.6.5AE"
+    }
+};
